5cb52ce7f71f440c955ceab92fe566c6fece5975
Luciano-AOSC <luciano.gatti.one@gmail.com>
2025-07-18 10:47:24 -0300

    Update DataBook.java

--- LiterAlura/src/main/java/com/aluraCourse/literAlura/model/DataBook.java
+++ LiterAlura/src/main/java/com/aluraCourse/literAlura/model/DataBook.java
@@ -5,26 +5,22 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 /**
- * Representa un libro recibido desde una API externa, como la de Project Gutenberg.
- * Esta clase utiliza anotaciones de Jackson para mapear automáticamente los campos JSON
- * a los atributos correspondientes del objeto.
- *
- * Campos destacados:
- * - id: identificador único del libro.
- * - title: título del libro.
- * - summaries: resúmenes opcionales del contenido.
- * - authors: lista de autores del libro.
- * - traslator: lista de traductores (¡ojo con el typo, debería ser "translators").
- * - copyright: indica si el libro está protegido por derechos de autor.
- * - downloadCount: cantidad de veces que fue descargado.
- * - format: distintos formatos disponibles para el libro (epub, html, imagen, etc.).
- *
- * Se usa la anotación @JsonIgnoreProperties(ignoreUnknown = true) para ignorar
- * cualquier campo no mapeado del JSON, permitiendo mayor flexibilidad frente a
- * cambios en la API externa.
- *
- * @author Luciano E. Gatti Flekenstein
- */
+* Representa un libro recibido desde una API externa, como la de Project Gutenberg.
+* Esta clase utiliza anotaciones de Jackson para mapear automáticamente los campos JSON
+* a los atributos correspondientes del objeto.
+*
+* Campos destacados:
+* - title: título del libro.
+* - authors: lista de autores del libro.
+* - languages: lista de lenguanjes en los que se encuantra el libro.
+* - downloadCount: cantidad de veces que fue descargado.
+*
+* Se usa la anotación @JsonIgnoreProperties(ignoreUnknown = true) para ignorar
+* cualquier campo no mapeado del JSON, permitiendo mayor flexibilidad frente a
+* cambios en la API externa.
+*
+* @author Luciano E. Gatti Flekenstein
+*/
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DataBook(
