0ee66dbddc15c5e5d3a642886e2dd0a76d2a1102
Luciano-AOSC <luciano.gatti.one@gmail.com>
2025-07-18 10:47:33 -0300

    Update Book.java

--- LiterAlura/src/main/java/com/aluraCourse/literAlura/model/entities/Book.java
+++ LiterAlura/src/main/java/com/aluraCourse/literAlura/model/entities/Book.java
@@ -1,11 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.aluraCourse.literAlura.model.entities;
 
 import com.aluraCourse.literAlura.model.DataBook;
-import com.aluraCourse.literAlura.model.entities.Person;
+import com.aluraCourse.literAlura.model.entities.Author;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,28 +12,40 @@ import jakarta.persistence.Table;
 import java.util.List;
 
 /**
- *
- * @author usuario
- */
+* Entidad que representa un libro almacenado en la base de datos.
+* Contiene información como el título, los idiomas disponibles, la cantidad de descargas
+* y su autor.
+*
+* Se encuentra mapeada a la tabla "book" mediante JPA.
+*
+* @author Luciano E. Gatti Flekenstein
+*/
 
 @Entity
 @Table(name = "book")
 public class Book {
+    //Atributos de la entidad
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
     private String title;
+    private Double downloadCount;
     
     @ElementCollection
     private List<String> languages;
-    
-    private Double downloadCount;
-    
+
     @ManyToOne
-    private Person author;
+    private Author author;
     
+    //Constructor por defecto requerido por JPA.
     public Book(){}
 
+    /**
+    * Constructor que inicializa un libro a partir de un objeto {@link DataBook},
+    * extrayendo el título, las descargas (si no puede parsear, asigna 0.0) y los idiomas.
+    *
+    * @param dataBook el objeto con los datos extraídos desde la API.
+    */
     public Book(DataBook dataBook) {
         this.title = dataBook.title();
         try{
@@ -48,6 +56,14 @@ public class Book {
         this.languages = dataBook.languages();
     }
 
+    @Override
+    public String toString() {
+        return "\n" + "Libro: " + "\n" +
+                "Title='" + title + "\n" +
+                "Descargas=" + downloadCount + "\n";
+    }
+    
+    //Getters y Setters
     public Long getId() {
         return Id;
     }
@@ -64,11 +80,11 @@ public class Book {
         this.title = title;
     }
 
-    public Person getAuthor() {
+    public Author getAuthor() {
         return author;
     }
 
-    public void setAuthor(Person author) {
+    public void setAuthor(Author author) {
         this.author = author;
     }
 
@@ -87,12 +103,4 @@ public class Book {
     public void setDownloadCount(Double downloadCount) {
         this.downloadCount = downloadCount;
     }
-    
-    @Override
-    public String toString() {
-        return "\n" + "Libro: " + "\n" +
-                "Title='" + title + "\n" +
-                "Descargas=" + downloadCount + "\n";
-    }
-
 }
